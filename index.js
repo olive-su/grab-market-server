3f0510351572042966339804d3e30387cf80b182
olive-su <1466su@naver.com>
2021-08-12 20:11:11 +0900

    server init

---
+++ index.js
@@ -0,0 +1,27 @@
+let http = require("http");
+let hostname = "127.0.0.1";
+let port = 8080;
+
+const server = http.createServer(function (req, res) {
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" }); // json 타입으로 응답을 넘기겠다는 뜻
+      const products = JSON.stringify([
+        {
+          name: "농구공",
+          price: 5000,
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+      res.end("생성되었습니다!");
+    }
+    res.end("Good Bye");
+  }
+});
+
+server.listen(port, hostname); // 해당 포트번호와 호스트 네임으로 호출을 기다리고 있음을 뜻함
+
+console.log("grab market server on!");
